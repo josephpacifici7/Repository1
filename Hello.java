e55d392d42fb5be9b21e7bd9d0b3c20265005638
josephpacifici7 <joseph.pacifici7@gmail.com>
2020-03-09 11:49:29 -0400

    What's up

---
+++ Hello.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // Bring in new scanner for user input
+        Scanner scan = new Scanner(System.in);
+        // Strings for user inputs
+        String userName;
+        String userAge;
+        String userHome;
+
+        // Automated conversation
+        System.out.print("Hello. What is your name?");
+        userName = scan.next();
+        System.out.print("It's nice to meet you, " + userName + ". How old are you?");
+        userAge = scan.next();
+        System.out.println("I see that you are still quite young at only " + userAge + ".");
+        System.out.print("Where do you live?");
+        userHome = scan.next();
+        System.out.println("Wow! I've always wanted to go to " + userHome + ". Thanks for chatting with me. Bye!");
+    }
+}
